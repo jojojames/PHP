53fc898474aae239118047fc4caa49671290fe84
James Nguyen <ja.nguyen@gmail.com>
2013-03-24 18:31:20 -0700

    wg

---
+++ widget_corp/staff.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Widget Corp</title>
+    <link media="all" href="stylesheets/public.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
+      <table cellspacing="0" id="structure">
+        <tr>
+          <td id="navigation">&nbsp</td>
+          <td id="page">
+            <h2>Staff Menu</h2>
+            <p>Welcome to the staff area.</p>
+            <ul>
+              <li>
+                <a href="content.php">Manage Website</a>
+              </li>
+              <li>
+                <a href="new_user.php">Add Staff User</a>
+              </li>
+              <li>
+                <a href="logout.php">Logout</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div id="footer">Copyright 2013, Widget Corp</div>
+  </body>
+</html>
